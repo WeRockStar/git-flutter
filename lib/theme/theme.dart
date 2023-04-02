ef342f2e7a436e879373642b2656921e0af5784a
WeRockStar <kotchaphan.m@gmail.com>
2023-04-02 18:49:08 +0700

    Add google font

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:git/theme/color.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-final theme = ThemeData(primarySwatch: primarySwatch);
+final theme = ThemeData(
+    primarySwatch: primarySwatch, textTheme: GoogleFonts.robotoMonoTextTheme());
